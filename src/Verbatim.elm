ac9f7b00f7af49e78f617d95f92acef4cb9ac3e1
amblafont <chaster_killer@hotmail.fr>
2026-05-07 10:36:05 +0200

    Verbatim labels now starts with "%verb " instead of \coqverb

--- src/Verbatim.elm
+++ src/Verbatim.elm
@@ -3,20 +3,29 @@ module Verbatim exposing (extractVerbatim, makeVerbatimLabel, removeVerbatim)
 removeVerbatim : String -> String
 removeVerbatim s = extractVerbatim s |> Maybe.withDefault s
 
+-- extractOldVerbatim : String -> Maybe String
+-- extractOldVerbatim s =
+--    let prefix = verbatimCmd ++ "{" in
+--    if String.startsWith prefix s && String.endsWith "}" s then
+--       Just (String.slice (String.length prefix) (-1) s)
+--    else
+--       Nothing
+
+-- verbatimOldCmd = "\\coqverb"
+verbatimCmd = "%verb "
+
 extractVerbatim : String -> Maybe String
 extractVerbatim s =
-   let prefix = verbatimCmd ++ "{" in
-   if String.startsWith prefix s && String.endsWith "}" s then
-      Just (String.slice (String.length prefix) (-1) s)
+   let prefix = verbatimCmd in
+   if String.startsWith prefix s then
+      Just (String.dropLeft (String.length prefix) s)
    else
       Nothing
-
-verbatimCmd = "\\coqverb"
 
 makeVerbatimLabel : Bool -> String -> String
 makeVerbatimLabel isVerbatim s =
    if isVerbatim then
-      verbatimCmd ++ "{" ++ s ++ "}"
+      verbatimCmd ++ s
    else
       s
 
